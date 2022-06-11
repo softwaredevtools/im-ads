d536698a13b7c5832c762109cd6468bcf91c01eb
Irwan Manurung <irwan21291@gmail.com>
2022-06-11 23:34:23 +0700

    ImAds Development tools

--- app/src/main/java/com/softwaredevtools/imads/App.java
+++ app/src/main/java/com/softwaredevtools/imads/App.java
@@ -3,6 +3,9 @@ package com.softwaredevtools.imads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.InterfaceAddress;
 
 public class App extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class App extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+
+        Log.i("ads","log");
+
     }
 }
