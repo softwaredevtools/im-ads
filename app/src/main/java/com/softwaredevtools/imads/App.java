22a1d3b4454391481e0cb3146d4b3e9e9f936f05
Irwan Manurung <irwan21291@gmail.com>
2022-06-11 23:16:43 +0700

    ImAds Development tools

---
+++ app/src/main/java/com/softwaredevtools/imads/App.java
@@ -0,0 +1,14 @@
+package com.softwaredevtools.imads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+    }
+}
